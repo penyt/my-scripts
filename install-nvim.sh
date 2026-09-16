6f23a1a9c8a16e3131a99d2476fd4fee08412860
Penyt <160297029+penyt@users.noreply.github.com>
2026-09-16 16:42:17 +0800

    3

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
